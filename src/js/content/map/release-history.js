be255dc09bffb36c0511cbba6cbce0f76f67b18d
suyankovsky <42886849+suyankovsky@users.noreply.github.com>
2019-10-14 22:44:01 +0900

    v2.2.1 (#4)

--- src/js/content/map/release-history.js
+++ src/js/content/map/release-history.js
@@ -1,41 +1,42 @@
 export default [
     {
-        version: '2.2',
+        version: "2.2.1",
         //detail_url: '',
-        date: '2019/8/12',
+        date: "2019/10/14",
+        points: ["起動しないバグを修正"],
+        develop: ["なぜか$.filterが動いてなかったので$.findにしたら直った"]
+    },
+    {
+        version: "2.2",
+        //detail_url: '',
+        date: "2019/8/12",
         points: [
-            'ニコニ広告のリンクからうまくプレイヤーを起動できない不具合を修正。',
-            '動画情報パネルで、動画IDとスレッドIDを表示するようにした（パネル最下部）',
-            '非ログイン時で見れないところを塞いだ（視聴履歴・ニコレポ・マイリスト登録ボタンなど）',
-            '動画情報パネルの文字色等々、一部見づらかった場所のコントラストを高くした',
+            "ニコニ広告のリンクからうまくプレイヤーを起動できない不具合を修正。",
+            "動画情報パネルで、動画IDとスレッドIDを表示するようにした（パネル最下部）",
+            "非ログイン時で見れないところを塞いだ（視聴履歴・ニコレポ・マイリスト登録ボタンなど）",
+            "動画情報パネルの文字色等々、一部見づらかった場所のコントラストを高くした"
         ],
         develop: [
-            'リリース履歴を切り出してそこからmanifest.jsonを更新するようにした',
-            '.editorconfigを追加した',
-        ],
+            "リリース履歴を切り出してそこからmanifest.jsonを更新するようにした",
+            ".editorconfigを追加した"
+        ]
     },
     {
-        version: '2.1',
+        version: "2.1",
         //detail_url: '',
-        date: '2019/7/28',
-        points: [
-            '動画の反転機能を追加。デザインの微修正。',
-        ],
+        date: "2019/7/28",
+        points: ["動画の反転機能を追加。デザインの微修正。"]
     },
     {
-        version: '2.0',
-        detail_url: 'https://ch.nicovideo.jp/article/ar1778305',
-        date: '2019/6/23',
-        points: [
-            '全面的に変えた。',
-        ],
+        version: "2.0",
+        detail_url: "https://ch.nicovideo.jp/article/ar1778305",
+        date: "2019/6/23",
+        points: ["全面的に変えた。"]
     },
     {
-        version: '1.0',
-        detail_url: 'https://ch.nicovideo.jp/article/ar1681485',
-        date: '2018/10/8',
-        points: [
-            '初回リリース。',
-        ],
-    },
+        version: "1.0",
+        detail_url: "https://ch.nicovideo.jp/article/ar1681485",
+        date: "2018/10/8",
+        points: ["初回リリース。"]
+    }
 ];
